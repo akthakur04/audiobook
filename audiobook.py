015dbd66a2c0861120f77fc5471a099a881e39e4
akshay thakur <akshay.181004@gmail.com>
2020-09-21 01:08:48 +0530

    first commit

--- audiobook.py
+++ audiobook.py
@@ -1,9 +1,9 @@
 import pyttsx3
-import PyPDF2
+
 import PyPDF4
 from tkinter.filedialog import *
 book = askopenfilename()
-#pdfreader =PyPDF2.PdfFileReader(book)
+
 pdfreader =PyPDF4.PdfFileReader(book)
 pages = pdfreader.numPages
 for num in range (0, pages):
